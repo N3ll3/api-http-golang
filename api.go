77677dce5455ac571e71285711fbb51175744472
unknown <nelleonnet@gmail.com>
2023-10-25 16:19:59 +0200

    correction connection bdd + route ping avec test conexion bdd

--- api.go
+++ api.go
@@ -1,25 +1,11 @@
 package main
 
 import (
-	db "api-http/DB"
+	database "api-http/db"
 	"encoding/json"
 	"log"
 	"net/http"
 )
-
-func getArtistsHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-        w.WriteHeader(http.StatusMethodNotAllowed)
-        return
-  }
-	result := db.GetArtists();
-	jsonStr, err := json.Marshal(result)
-	if err != nil {
-		 w.WriteHeader(http.StatusInternalServerError)
-     return
-	}
-  w.Write([]byte(jsonStr))
-}
 
 func apiKeyMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -29,7 +15,22 @@ func apiKeyMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+func getArtistsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+        w.WriteHeader(http.StatusMethodNotAllowed)
+        return
+  }
+	result := database.GetArtists();
+	jsonStr, err := json.Marshal(result)
+	if err != nil {
+		 w.WriteHeader(http.StatusInternalServerError)
+     return
+	}
+  w.Write([]byte(jsonStr))
+}
+
 func pingHandler(w http.ResponseWriter, r *http.Request) {
+	database.Connection()
 	 w.Write([]byte("PONG"))
 }
 
--- db/database.go
+++ db/database.go
@@ -1,6 +1,47 @@
-package db
+package database
 
-import "log"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+func Connection() {
+	log.Println("Preparation de la connexion DB")
+	godotenv.Load(".env")
+ var (
+  host     = os.Getenv("DBHOST")
+  port     = os.Getenv("DBPORT")
+  user     = os.Getenv("DBUSER")
+  password = os.Getenv("DBPASS")
+  dbname   = os.Getenv("DBNAME")
+	sslmode  = os.Getenv("DBSSLMODE")
+	certifPath 	 = os.Getenv("CERTIFCAPATH")
+	)
+	postgresqlDbInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+    "password=%s dbname=%s sslmode=%s sslrootcert=%s",
+    host, port, user, password, dbname, sslmode, certifPath) 
+
+	db, err := sql.Open("postgres", postgresqlDbInfo)
+  if err != nil {
+    log.Fatal(err)
+  }
+	if db == nil {
+    log.Fatal("Database connection is nil")
+	}	
+  defer db.Close()
+  err = db.Ping()
+  if err != nil {
+    log.Fatal(err)
+  }
+  log.Println("Established a successful connection!")
+}
 
 type Artist struct {
 	Id     string
@@ -14,6 +55,7 @@ type Track struct {
 }
 
 func GetArtists() []Artist {
+	log.Println("GetArtists")
 	rows, err := db.Query(
 		`WITH ArtistTracks AS (
     SELECT sa.name AS artist,
